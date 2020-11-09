bcb9fe024e5a2213f26a4c5e518e2aef1576dde0
chriscushway <chriscushway@gmail.com>
2020-11-09 21:06:22 +0000

    added code to setup buzzer

--- Terrarium.py
+++ Terrarium.py
@@ -1,7 +1,25 @@
+import busio
+import digitalio
+import time
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import threading
+import RPi.GPIO as GPIO
+
+GPIO.setup(17, GPIO.OUT)
+
+value = True
+
 if __name__ == "__main__":
     try:
-        print('hello world')
+        while True:
+            time.sleep(0.5)
+            value = False if value else True
+            print(value)
+            GPIO.output(17,value)
+
     except Exception as e:
         print(e)
     finally:
-        print('Goodbye')
+        GPIO.cleanup()
