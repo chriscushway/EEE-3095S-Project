5f790c6e9aac8bb55f3a2707f98dd587be2b99bc
chriscushway <chriscushway@gmail.com>
2020-11-11 02:03:23 +0000

    added code to store values

--- Terrarium.py
+++ Terrarium.py
@@ -11,8 +11,9 @@ import threading
 import RPi.GPIO as GPIO
 
 # Set of globals
-buzzer = 17         # We are using BCM numbering convention
+buzzer = 22         # We are using BCM numbering convention
 stop_button = 27
+toggle_button = 17
 
 # This will be used to determine whether the system should stop monitoring the terrarium environment
 # It is defaulted to false as initially the system is monitoring
@@ -38,8 +39,10 @@ eeprom = ES2EEPROMUtils.ES2EEPROM()
 
 def setup_stop_button():
     GPIO.setup(stop_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(toggle_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     # Detect rising edge since we are using a pull down resistor
     GPIO.add_event_detect(stop_button, GPIO.RISING, callback=stop_start_monitoring, bouncetime=200)
+    GPIO.add_event_detect(toggle_button, GPIO.RISING, callback=print_samples, bouncetime=200)
 
 # Function which will either stop or start monitoring the terrarium environment
 def stop_start_monitoring(channel):
@@ -57,42 +60,29 @@ def trigger_buzzer():
     time.sleep(0.2)
     GPIO.output(buzzer, False)
 
-# We store the amount of samples that are in the EEPROM memory in the first byte
-def fetch_sample_count():
-    return eeprom.read_byte(0)
-
-def update_sample_count(sample_count):
-    eeprom.write_block(0, [sample_count])
-
-def store_sample(sample_value):
-    old_data = fetch_samples()
-    num_samples = fetch_sample_count()
-    print('there are ' + str(num_samples) + ' samples')
-    new_data = old_data
-
-    if (num_samples < 20):
-        num_samples += 1
-        new_data.append(num_samples)
-    else:
-        new_data = old_data[1:len(old_data)]
-    update_sample_count(num_samples)
-    eeprom.write_block(1,new_data)
+def store_sample(hour, minute, second, temp):
+    old_data  = fetch_samples()
+    old_data.extend([hour,minute,second,temp])
+    old_data = old_data[4:len(old_data)] #slice off first item
+    eeprom.write_block(0,old_data)
 
 def fetch_samples():
-    num_samples = fetch_sample_count()
-    scores = eeprom.read_block(1, 16*4)
-    return scores
+    # Get the samples
+    samples = eeprom.read_block(0, 20*4)
+    # return back the results
+    return samples
 
-def print_samples(raw_data):
-    print('raw data is printing')
-    print(raw_data)
-    for i in range(0,len(raw_data),4):
-        print(str(raw_data[i]) + str(raw_data[i + 1]) + str(raw_data[i + 2]) + str(raw_data[i + 3]))
+def print_samples(channel):
+    print(fetch_samples())
 
 # function to convert the voltage on the adc channel into temperature in C
 def calculate_temp():
     global chan
     return (chan.voltage - 0.5)/0.01
+
+# function that returns the hour, minute and second of a time string with format HH:MM:SS
+def get_time_values(time):
+    return time[0:2],time[3:5],time[6:8]
 
 def setup_buzzer():
     GPIO.setup(buzzer, GPIO.OUT)
@@ -112,18 +102,17 @@ def read_temp_value():
                 else:
                     print_output(calculate_temp(), '')
                 samples_printed += 1
+                
             time.sleep(1)
 
         
-        
-
-
 def print_output(temp, buzzer=''):
     curr_time = time.localtime() 
     curr_clock = time.strftime("%H:%M:%S", curr_time)
     syst_time = time.time() - sys_time
     syst_time = time.strftime('%H:%M:%S', time.gmtime(syst_time))
-
+    hour,minute,second = get_time_values(syst_time)
+    store_sample(int(hour), int(minute), int(second), int(temp))
     print('{0}  {1}   {2:.0f} C {3}'.format(curr_clock, syst_time, temp, buzzer))
     
     
